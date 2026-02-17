fb32c416e8c9a14318c72605e8f1a48494df2eae
ninzzd <ninaad.desai1212@gmail.com>
2026-02-17 12:32:58 +0530

    Implemented and tested upsampler

---
+++ upsampler/upsampler.v
@@ -0,0 +1,30 @@
+/*
+    Description: An L-stage upsampler for W-bit inputs
+*/
+module upsampler
+#(
+    parameter W = 16,
+    parameter L = 2
+)(
+    input clk,
+    input rst,
+    input [W-1:0] in,
+    output [W-1:0] out
+);
+    parameter N = $clog2(L);
+    reg [N-1:0] counter;
+    reg [W-1:0] temp;
+    wire flag = ~|(counter); // ~(counter[N-1] OR coutner[N-2] OR ... OR counter[0]) (zero state)
+    always @(posedge clk) begin
+        if(rst)
+        begin
+            counter <= {N{1'b1}};
+        end
+        else
+        begin
+            counter <= counter + 1;
+        end
+        temp <= in; // store input in temp register
+    end
+    assign out = flag ? temp : 0; // if flag is 1, output in, else output 0
+endmodule
---
+++ upsampler/upsampler_tb.v
@@ -0,0 +1,42 @@
+/*
+    To run: 
+        iverilog -o upsampler_tb.vvp upsampler/upsampler_tb.v upsampler/upsampler.v
+        vvp upsampler_tb.vvp
+        gtkwave upsampler_tb.vcd
+*/
+module upsampler_tb;
+    reg clk;
+    reg rst;
+    reg [15:0] in;
+    wire [15:0] out;
+
+    upsampler #(.W(16), .L(2)) uut (
+        .clk(clk),
+        .rst(rst),
+        .in(in),
+        .out(out)
+    );
+
+    // Clock generation
+    always #5 clk = ~clk; // Toggle clock every 5 time units (100 MHz)
+
+    initial begin
+        $dumpfile("upsampler_tb.vcd");
+        $dumpvars(0, upsampler_tb);
+        // Initialize signals
+        clk = 0;
+        rst = 1;
+        in = 16'h0000;
+
+        // Release reset after some time
+        #10 rst = 0;
+
+        // Apply test vectors
+        in = 16'h1234; // First input
+        #10 in = 16'h5678; // Second input after some cycles
+        #10 in = 16'h9ABC; // Third input after some cycles
+
+        // Finish simulation after some time
+        #25 $finish;
+    end
+endmodule
